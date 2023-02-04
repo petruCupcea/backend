4081b7e322f20c2d55904d47986d503356a1bd03
Cupcea Petru <petru.cupcea@getik.net>
2023-02-04 17:07:35 +0200

    added core dependencies for request module

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:8100',
+    origin: 'http://localhost:4200',
   });
   await app.listen(3000, '0.0.0.0', () => {
     console.log('Listening on port 3000');
   });
 }
-bootstrap();
+bootstrap().then();
